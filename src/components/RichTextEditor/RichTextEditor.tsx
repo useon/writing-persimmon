3df85f64616cbfaa2a22a8be051f04b721a234aa
useon <yoosk4986@naver.com>
2023-08-23 17:06:33 +0900

    feat: 글쓰기 에디터 컴포넌트 구현

---
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import React, { useRef } from 'react';
+
+import { Editor } from '@tinymce/tinymce-react';
+import { Editor as TinyMCEEditor } from 'tinymce';
+
+import Button from '../common/Button/Button';
+
+const RichTextEditor = () => {
+  const editorRef = useRef<TinyMCEEditor | null>(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+  return (
+    <>
+      <Editor
+        apiKey={process.env.EDITOR_API_KEY}
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue='<p>This is the initial content of the editor.</p>'
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'preview',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'code',
+            'help',
+            'wordcount',
+          ],
+          toolbar:
+            'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+      <Button onClick={log}>게시하기</Button>
+    </>
+  );
+};
+
+export default RichTextEditor;
