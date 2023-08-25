9aede01ef7a5ce433817c5a9011792894a29467a
useon <yoosk4986@naver.com>
2023-08-25 16:38:29 +0900

    feat: 글쓰기 미리보기 컴포넌트 구현

---
+++ src/components/WritingPreview/WritingPreview.tsx
@@ -0,0 +1,23 @@
+import { styled } from 'styled-components';
+
+import { WritingType } from '@/types/writingType';
+
+interface Props {
+  writingData: WritingType;
+}
+
+const WritingPreview = ({ writingData }: Props) => {
+  return (
+    <>
+      <TitleArea>{writingData.title}</TitleArea>
+      <div dangerouslySetInnerHTML={{ __html: writingData.body }}></div>
+    </>
+  );
+};
+
+export default WritingPreview;
+
+const TitleArea = styled.h1`
+  padding: 20px;
+  font-size: 2.5rem;
+`;
